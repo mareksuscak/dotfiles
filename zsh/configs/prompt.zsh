033345deec0eac48f1e35ba5aa0292e85fe875fc
Marek Suscak <marek.suscak@gmail.com>
2017-10-08 16:12:33 +0200

    Configure the right prompt to display vi mode indicator.

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -7,3 +7,13 @@ git_prompt_info() {
 }
 setopt promptsubst
 PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
+
+# modify the right prompt to contain vi mode indicator
+function zle-line-init zle-keymap-select {
+    VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
+    RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
+    zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
