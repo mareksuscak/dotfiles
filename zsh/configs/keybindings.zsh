03282b0669c14ef29c69b751fd334ece79f3d7d6
Marek Suscak <marek.suscak@gmail.com>
2017-10-08 16:11:25 +0200

    Add extra keybindings that fix common behavior.

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -15,3 +15,26 @@ bindkey "^Y" accept-and-hold
 bindkey "^N" insert-last-word
 bindkey "^Q" push-line-or-edit
 bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
+
+# backspace and ^h working even after
+# returning from command mode
+bindkey "^?" backward-delete-char
+bindkey '^h' backward-delete-char
+
+# ctrl-w removed word backwards
+bindkey '^w' backward-kill-word
+
+# ctrl+u removes line backwards
+bindkey "^U" backward-kill-line
+
+# fn+left (home), fn+right (end) and fn+backspace
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[3~" delete-char
+
+# mapping alt and ctrl combinations
+bindkey "^[^?" backward-kill-word
+bindkey "^[[1;3C" forward-word
+bindkey "^[[1;3D" backward-word
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
