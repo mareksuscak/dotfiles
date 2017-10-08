8060186f0d998cf7f0bdc049ef88fcd24cb43c42
Marek Suscak <marek.suscak@gmail.com>
2017-10-08 16:22:44 +0200

    Make rprompt work on the initial load.

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -9,11 +9,16 @@ setopt promptsubst
 PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
 
 # modify the right prompt to contain vi mode indicator
+function vi_mode_info() {
+  VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
+  RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
+}
+
 function zle-line-init zle-keymap-select {
-    VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
-    RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
+    vi_mode_info
     zle reset-prompt
 }
 
 zle -N zle-line-init
 zle -N zle-keymap-select
+vi_mode_info
