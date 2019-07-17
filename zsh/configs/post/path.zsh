da775998fda7e0a223870b8798be7daa988c7634
Marek Suscak <marek.suscak@gmail.com>
2019-07-17 12:27:15 +0200

    Add comment

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,7 +1,8 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
-# load ASDF, falling back to rbenv if not available
+# TODO change condition?
+# load ASDF from brew folder, falling back to rbenv if not available
 if [ -d "$HOME/.asdf" ]; then
   . $HOME/.asdf/asdf.sh
 fi
